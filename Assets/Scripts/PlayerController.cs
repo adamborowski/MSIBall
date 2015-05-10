f0d2af0968ebe5b19c6444b313abdeb6fc625a9a
adamborowski <fl.borovsky@gmail.com>
2015-05-10 15:52:25 +0200

    cleaning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	
 	public float speed;
+	public float acceleration=1;
 	
 	private Rigidbody rb;
 	public static float distanceTraveled;
@@ -21,10 +22,9 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		//float moveVertical = 0.0f;
 		
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f,acceleration*0.4f);
 		rb.AddForce (movement * speed);
-
+		//rb.MovePosition ();
 		distanceTraveled = transform.localPosition.z;
 	}
 }
