beb112946b38d24c93a94dbc433761bfa816723f
Radek Pieczątkiewicz <pieczatek@wp.pl>
2015-05-30 12:32:33 +0200

    nowa scena z prostym menu

---
+++ Assets/Scripts/ErfInitialization.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using Erf;
+using System.IO;
+using System;
+public class ErfInitialization : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    void Awake() { 
+        ErfLogger.SetLoggingEnabled (true);
+        ErfLogger.SetAllStreamsToLoggingFile (@"d://debug.txt");
+        Debug.Log ("Initializing ERF for ErfDemo, trying to load "+Directory.GetCurrentDirectory()+"\\ExternalComponentLibrary.dll");
+
+        ErfContext context = ErfContext.GetInstance ();
+        try {
+            ExternalComponentLibrary library = ErfContext.GetInstance ().LoadComponentLibrary ("ExternalComponentLibrary.dll");
+            if (library == null) { 
+                Debug.LogError("Could not load ExternalComponentLibrary library");
+                return;
+            }
+        } catch (Exception e)
+        {
+            Debug.Log (e.Message);
+        }
+        
+        testMSIBallErf ();
+        
+        Debug.Log ("Library successfully loaded");
+    }
+    
+    void testMSIBallErf()
+    {
+        try {
+            ExternalComponentLibrary library = ErfContext.GetInstance ().LoadComponentLibrary ("MSIBallErf.dll");
+            if (library == null) { 
+                Debug.LogError("Could not load MSIBallErf library");
+                return;
+            }
+        } catch (Exception e)
+        {
+            Debug.Log (e.Message);
+            return;
+        }
+        ExternalExpert msiBallEmotionModel = ErfContext.GetInstance ().FindExpert ("MSIBallEmotionModel");
+        CharacterModel playerModel = new CharacterModel ();
+        playerModel.RegisterExpert (msiBallEmotionModel);
+        
+        EecEvent timeElapsedEvent = new EecEvent ((int)GameEvent.STD_TIME_ELAPSED);
+        timeElapsedEvent.AddValue ("NO_OF_COLLISIONS", Variant.Create (2));
+        timeElapsedEvent.AddValue ("CURRENT_SPEED", Variant.Create (7.0f));
+        
+        playerModel.HandleEvent (timeElapsedEvent);
+        
+        float fear = playerModel.GetEmotionVector ().GetValue (OccEmotions.FEAR).AsFloat();
+        
+        if(fear > 0.0f)
+            Debug.Log ("FIRST I WAS AFRAID, I WAS PETRIFIED!");
+        else
+            Debug.Log ("I AM FEARLESS! I AM DEATH! I AM FIRE!");
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+	public GUISkin guiSkin;
+	public Texture2D background, LOGO;
+	public bool DragWindow = false;
+	
+	
+	private string clicked = "";
+	private Rect WindowRect = new Rect((Screen.width / 2) - 100, Screen.height / 2, 200, 200);
+	
+	private void Start()
+	{
+
+	}
+
+	private void OnGUI()
+	{
+
+		if (background != null)
+			GUI.DrawTexture(new Rect(0,0,Screen.width , Screen.height),background);
+		if (LOGO != null)
+			GUI.DrawTexture(new Rect((Screen.width / 2) - 100, 30, 200, 200), LOGO);
+		
+		GUI.skin = guiSkin;
+		if (clicked == "")
+		{
+			WindowRect = GUI.Window(0, WindowRect, menuFunc, "Main Menu");
+		}
+
+	}
+
+	
+	private void menuFunc(int id)
+	{
+
+		GUILayout.Label ("z emocjami:");
+		if (GUILayout.Button("Graj"))
+		{
+			clicked = "emotions";
+			Application.LoadLevel("BallGame");
+		}
+
+		GUILayout.Label ("bez emocji:");
+		if (GUILayout.Button("Latwy"))
+		{
+			clicked = "easy";
+			Application.LoadLevel("BallGame");
+		}
+		if (GUILayout.Button("Sredni"))
+		{
+			clicked = "medium";
+			Application.LoadLevel("BallGame");
+		}
+		if (GUILayout.Button("Trudny"))
+		{
+			clicked = "hard";
+			Application.LoadLevel("BallGame");
+		}
+
+		GUILayout.Space(10);
+		if (GUILayout.Button("Wyjdz"))
+		{
+			Application.Quit();
+		}
+
+		if (DragWindow)
+			GUI.DragWindow(new Rect(0, 0, Screen.width, Screen.height));
+
+	}
+	
+	private void Update()
+	{
+		if (clicked == "about" && Input.GetKey (KeyCode.Escape))
+			clicked = "";
+	}
+}
